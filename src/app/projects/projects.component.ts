d274214d89db82b0824b64b58b9e59df3ca74ae4
Daniel Atonal <danielatonal8@gmail.com>
2023-05-02 13:40:18 -0600

    imagenes de mitikah

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -31,30 +31,107 @@ export class ProjectsComponent {
       alt: 'mitikah',
     }, 
     {
-      image: 'assets/images/projects/mitikah/4.png',
-      thumbImage: 'assets/images/projects/mitikah/1.png',
+      image: 'assets/images/projects/mitikah/5.png',
+      thumbImage: 'assets/images/projects/mitikah/5.png',
       alt: 'mitikah',
     }, 
     {
-      image: 'assets/images/projects/mitikah/1.png',
-      thumbImage: 'assets/images/projects/mitikah/1.png',
+      image: 'assets/images/projects/mitikah/6.png',
+      thumbImage: 'assets/images/projects/mitikah/6.png',
       alt: 'mitikah',
     }, 
     {
-      image: 'assets/images/projects/mitikah/1.png',
-      thumbImage: 'assets/images/projects/mitikah/1.png',
+      image: 'assets/images/projects/mitikah/7.png',
+      thumbImage: 'assets/images/projects/mitikah/7.png',
       alt: 'mitikah',
     }, 
     {
-      image: 'assets/images/projects/mitikah/1.png',
-      thumbImage: 'assets/images/projects/mitikah/1.png',
+      image: 'assets/images/projects/mitikah/8.png',
+      thumbImage: 'assets/images/projects/mitikah/8.png',
       alt: 'mitikah',
     }, 
     {
-      image: 'assets/images/projects/mitikah/1.png',
-      thumbImage: 'assets/images/projects/mitikah/1.png',
+      image: 'assets/images/projects/mitikah/9.png',
+      thumbImage: 'assets/images/projects/mitikah/9.png',
       alt: 'mitikah',
     }, 
+    {
+      image: 'assets/images/projects/mitikah/10.png',
+      thumbImage: 'assets/images/projects/mitikah/10.png',
+      alt: 'mitikah',
+    }, 
+    {
+      image: 'assets/images/projects/mitikah/11.png',
+      thumbImage: 'assets/images/projects/mitikah/11.png',
+      alt: 'mitikah',
+    }, 
+    {
+      image: 'assets/images/projects/mitikah/12.png',
+      thumbImage: 'assets/images/projects/mitikah/12.png',
+      alt: 'mitikah',
+    }, 
+    {
+      image: 'assets/images/projects/mitikah/13.png',
+      thumbImage: 'assets/images/projects/mitikah/13.png',
+      alt: 'mitikah',
+    }, 
+    {
+      image: 'assets/images/projects/mitikah/14.png',
+      thumbImage: 'assets/images/projects/mitikah/14.png',
+      alt: 'mitikah',
+    }, 
+    {
+      image: 'assets/images/projects/mitikah/15.png',
+      thumbImage: 'assets/images/projects/mitikah/15.png',
+      alt: 'mitikah',
+    }, 
+    {
+      image: 'assets/images/projects/mitikah/16.png',
+      thumbImage: 'assets/images/projects/mitikah/16.png',
+      alt: 'mitikah',
+    }, 
+    {
+      image: 'assets/images/projects/mitikah/instalacionDeTuberias.png',
+      thumbImage: 'assets/images/projects/mitikah/instalacionDeTuberias.png',
+      title:"Instalación de tuberias",
+      alt: 'mitikah',
+    }, 
+    {
+      image: 'assets/images/projects/mitikah/17.png',
+      thumbImage: 'assets/images/projects/mitikah/17.png',
+      alt: 'mitikah',
+    }, 
+    {
+      image: 'assets/images/projects/mitikah/18.png',
+      thumbImage: 'assets/images/projects/mitikah/18.png',
+      alt: 'mitikah',
+    }, 
+    {
+      image: 'assets/images/projects/mitikah/19.png',
+      thumbImage: 'assets/images/projects/mitikah/19.png',
+      alt: 'mitikah',
+    }, 
+    {
+      image: 'assets/images/projects/mitikah/20.png',
+      thumbImage: 'assets/images/projects/mitikah/20.png',
+      alt: 'mitikah',
+    }, 
+    {
+      image: 'assets/images/projects/mitikah/21.png',
+      thumbImage: 'assets/images/projects/mitikah/21.png',
+      alt: 'mitikah',
+    }, 
+    {
+      image: 'assets/images/projects/mitikah/22.png',
+      thumbImage: 'assets/images/projects/mitikah/22.png',
+      alt: 'mitikah',
+    }, 
+    {
+      image: 'assets/images/projects/mitikah/23.png',
+      thumbImage: 'assets/images/projects/mitikah/23.png',
+      alt: 'mitikah',
+    }, 
+    
   ];
 
 }
